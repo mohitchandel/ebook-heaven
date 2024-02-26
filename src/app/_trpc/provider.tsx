a0b0b557840b7d4176f2f0d659b0b9cb651e584d
Mohit Chandel <devmohit93@gmail.com>
2024-02-26 13:24:36 +0530

    fix: removed loacalhost

--- src/app/_trpc/provider.tsx
+++ src/app/_trpc/provider.tsx
@@ -14,7 +14,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: "/api/trpc",
         }),
       ],
     })
