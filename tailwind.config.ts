db12cbe63ed3a44d73ffb539b1736b270dbcb4a6
Mohit Chandel <devmohit93@gmail.com>
2024-02-26 15:05:23 +0530

    fix: components in tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import { nextui } from "@nextui-org/react";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
